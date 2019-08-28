3f41d5b0fd3ea5a0fa018a01169d68ed8196fd06
mbtodd <michaelbtodd@yahoo.com>
2019-08-28 00:02:03 -0400

    forms

--- src/app/products.component.ts
+++ src/app/products.component.ts
@@ -1,26 +1,32 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-products',
-  templateUrl: './products.component.html'
+	selector: 'app-products',
+	templateUrl: './products.component.html'
 })
 export class ProductsComponent {
-  productName = 'A Book';
-  isDisabled = true;
-  products = ['A Book', 'A Tree'];
+	productName = 'A Book';
+	isDisabled = true;
+	products = [
+		'A Book',
+		'A Tree'
+	];
 
-  constructor() {
-    setTimeout(() => {
-      // this.productName = 'A Tree';
-      this.isDisabled = false;
-    }, 3000);
-  }
+	constructor() {
+		setTimeout(() => {
+			// this.productName = 'A Tree';
+			this.isDisabled = false;
+		}, 3000);
+	}
 
-  onAddProduct() {
-    this.products.push(this.productName);
-  }
+	onAddProduct(form) {
+		// this.products.push(this.productName);
+		if (form.valid) {
+			this.products.push(form.value.productName);
+		}
+	}
 
-  onRemoveProduct(productName: string) {
-    this.products = this.products.filter(p => p !== productName);
-  }
+	onRemoveProduct(productName: string) {
+		this.products = this.products.filter(p => p !== productName);
+	}
 }
